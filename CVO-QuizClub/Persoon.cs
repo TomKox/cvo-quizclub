63cd0921700ac521eafefb6d823e412f3b8433c9
Tom Kox <kox.tom@gmail.com>
2019-05-15 22:02:00 +0200

    add class Persoon

---
+++ CVO-QuizClub/Persoon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVO_QuizClub
+{
+    public enum Geslacht { M, V, X }
+
+    public class Persoon
+    {
+        private DateTime _geboortedatum;
+
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+        public Geslacht Geslacht { get; set; }
+
+        public DateTime Geboortedatum { get => _geboortedatum; set => _geboortedatum = value; }
+        public int Leeftijd
+        {
+            get
+            {
+                DateTime nu = DateTime.Now;
+                int leeftijd = nu.Year - Geboortedatum.Year;
+                if (nu.Month < Geboortedatum.Month || (nu.Month == Geboortedatum.Month && nu.Day < Geboortedatum.Day))
+                    leeftijd--;
+                return leeftijd;
+            }
+        }
+    }
+}
