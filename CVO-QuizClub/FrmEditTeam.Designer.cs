3c8d84ebe98c0435f8efc4e4a17e05f89094a7f7
Tom Kox <tom.kox@tenforce.com>
2019-05-23 16:36:04 +0200

    add FrmEditTeam class

---
+++ CVO-QuizClub/FrmEditTeam.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CVO_QuizClub
+{
+    partial class FrmEditTeam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.txtVoornaam = new System.Windows.Forms.TextBox();
+            this.lblNaam = new System.Windows.Forms.Label();
+            this.txtNummer = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(234, 132);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 35);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Annuleren";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnConfirm.Location = new System.Drawing.Point(113, 132);
+            this.btnConfirm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(112, 35);
+            this.btnConfirm.TabIndex = 11;
+            this.btnConfirm.Text = "Toevoegen";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // txtVoornaam
+            // 
+            this.txtVoornaam.Location = new System.Drawing.Point(113, 60);
+            this.txtVoornaam.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtVoornaam.Name = "txtVoornaam";
+            this.txtVoornaam.Size = new System.Drawing.Size(249, 26);
+            this.txtVoornaam.TabIndex = 7;
+            // 
+            // lblNaam
+            // 
+            this.lblNaam.AutoSize = true;
+            this.lblNaam.Location = new System.Drawing.Point(54, 63);
+            this.lblNaam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNaam.Name = "lblNaam";
+            this.lblNaam.Size = new System.Drawing.Size(51, 20);
+            this.lblNaam.TabIndex = 10;
+            this.lblNaam.Text = "Naam";
+            // 
+            // txtNummer
+            // 
+            this.txtNummer.Location = new System.Drawing.Point(113, 20);
+            this.txtNummer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNummer.Name = "txtNummer";
+            this.txtNummer.ReadOnly = true;
+            this.txtNummer.Size = new System.Drawing.Size(85, 26);
+            this.txtNummer.TabIndex = 9;
+            this.txtNummer.TabStop = false;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(79, 23);
+            this.lblId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(26, 20);
+            this.lblId.TabIndex = 8;
+            this.lblId.Text = "ID";
+            // 
+            // FrmEditTeam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 222);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtVoornaam);
+            this.Controls.Add(this.lblNaam);
+            this.Controls.Add(this.txtNummer);
+            this.Controls.Add(this.lblId);
+            this.Name = "FrmEditTeam";
+            this.Text = "FrmEditTeam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.TextBox txtVoornaam;
+        private System.Windows.Forms.Label lblNaam;
+        private System.Windows.Forms.TextBox txtNummer;
+        private System.Windows.Forms.Label lblId;
+    }
+}
